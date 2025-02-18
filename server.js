61e36a9ee05c8e2b9b2170c2fad2145b9738a828
Bernardo <sardinhaber@gmail.com>
2025-02-18 10:58:16 -0300

    feat: project example

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+const usersRoutes = require("./routes/usersRoutes");
+app.use("/api", usersRoutes);
+
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+});
